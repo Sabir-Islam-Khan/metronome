cc9d57b510ec32863d444b6de330bea08d8dcc53
Sabir Islam Khan <sabir.islam14@gmail.com>
2024-12-08 20:27:12 +0600

    initial commit

---
+++ src/main/java/co/asterisklab/metronome/MetronomeApplication.java
@@ -0,0 +1,13 @@
+package co.asterisklab.metronome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetronomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetronomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/asterisklab/metronome/MetronomeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.asterisklab.metronome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetronomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
