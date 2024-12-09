35957160871c1603f58dc0f0727dc5a75636bdad
Sabir Islam Khan <sabir.islam14@gmail.com>
2024-12-09 22:27:29 +0600

    JWT setup complete

---
+++ src/main/java/co/asterisklab/metronome/configs/SecurityConfig.java
@@ -0,0 +1,4 @@
+package co.asterisklab.metronome.configs;
+
+public class SecurityConfig {
+}
---
+++ src/main/java/co/asterisklab/metronome/utils/JwtTokenUtil.java
@@ -0,0 +1,32 @@
+package co.asterisklab.metronome.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+}
